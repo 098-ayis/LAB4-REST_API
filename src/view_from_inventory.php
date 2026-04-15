062accbee311ec884e986868b8ee9e4280a4478d
098-ayis <lobarbioayis19@gmail.com>
2026-04-15 11:31:37 +0800

    Update view_from_inventory.php

--- src/view_from_inventory.php
+++ src/view_from_inventory.php
@@ -1,13 +1,18 @@
 <?php
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, OPTIONS");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-//database connection
-include 'db_connection.php';
+// Handle Preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    //database connection
+    include 'db_connection.php';
 
     $stmt = $conn->prepare("SELECT * FROM inventory");
     $stmt->execute();
