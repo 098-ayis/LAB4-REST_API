d00afd1f37f8b4dacd48f2f6c39f28db75463071
098-ayis <lobarbioayis19@gmail.com>
2026-04-15 11:50:29 +0800

    Update add_to_inventory.php

--- src/add_to_inventory.php
+++ src/add_to_inventory.php
@@ -4,6 +4,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //database connection
     include 'db_connection.php';
